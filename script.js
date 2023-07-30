35537d5cfab5d71e5d579fb5b3fa90d989a063b5
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-07-30 21:27:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,28 @@ item.addEventListener("click", () => {
 
     //   creating pop_up
   let pop = document.createElement("div");
-  pop.className = "popup";
+  pop.className = "popup-box";
 
-
+//heading
   let pop_upHead = document.createElement("div");
-  pop_upHead.id = "popupHead";
-
-
+  pop_upHead.id = "popupHeading";
   pop_upHead.textContent = heading;
+
+//input
   let popInput = document.createElement("input");
-  popInput.id = "popupInput";
+  popInput.id = "p_Input";
   popInput.type = "text";
-  popInput.placeholder = heading;
-
+ 
   let buttons = document.createElement("div");
-  buttons.className = "buttons";
-
+  buttons.className = "button";
+//add
   let pop_Add = document.createElement("button");
-  pop_Add.id = "popupAdd";
+  pop_Add.id = "Add_p";
   pop_Add.textContent = "Add";
 
+  //close
   let popClose = document.createElement("button");
-  popClose.id = "popupClose";
+  popClose.id = "Close_p";
   popClose.textContent = "Close";
 
   // add items to pop up
@@ -42,5 +42,44 @@ item.addEventListener("click", () => {
   buttons.appendChild(pop_Add);
   buttons.appendChild(popClose);
 
-}
 
+if (heading === "Add new subitems") {
+    pop_Add.addEventListener("click", () => {
+      creatingSubTask(popInput.value, parentNode);
+      close_Popup();
+    });
+  } else if (heading === "Add your items") {
+    pop_Add.addEventListener("click", () => {
+      createNewTask(popInput.value);
+     
+      Text_line.style.display = "none";
+      close_Popup();
+      pg1();
+    });
+  }
+
+  //close pop up
+  popClose.addEventListener("click", () => {
+    close_Popup();
+  });
+
+// for making bacground blur
+
+for (let i = 0; i < document.body.children.length; i++) {
+  document.body.children[i].style.filter = "blur(5px)";
+}
+pop.style.filter = "blur(0)";
+
+
+
+//close
+  function close_Popup() {
+    pop.remove();
+    popInput.value = "";
+
+    for (let i = 0; i < document.body.children.length; i++) {
+      document.body.children[i].style.filter = "blur(0)";
+    }
+  }
+
+}
