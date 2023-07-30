10dfc13a75f1264805e4e67de1c51158406e506b
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-07-30 18:43:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,182 +1,46 @@
-const add1=document.getElementById("popupbox")
-const cardCon=document.getElementById("cardcontainer")
-const newCardName=document.getElementById("newCardName")
-const popupbox2=document.getElementById("popupbox1")
-const head1=document.getElementById("head1")
+let item = document.getElementById("add_item");
+let Text_line = document.getElementById("Text_line");
+
+item.addEventListener("click", () => {
+    addItems("Add items");
+  });
+  
+  function addItems(heading, parentNode = "") {
+    console.log(heading);
+
+    //   creating pop_up
+  let pop = document.createElement("div");
+  pop.className = "popup";
 
 
-//p2
-//const carditem= document.getElementById("newCardName1")
-//const addList= document.getElementById("add1")
-//const delitem= document.getElementById("close1")
+  let pop_upHead = document.createElement("div");
+  pop_upHead.id = "popupHead";
 
-//function for add new task
 
-function addTask(){
- add1.classList.remove("hide")
- head1.style.display="none"
- var toggle =document.getElementById("main")
- toggle.classList.addTask("blur")
+  pop_upHead.textContent = heading;
+  let popInput = document.createElement("input");
+  popInput.id = "popupInput";
+  popInput.type = "text";
+  popInput.placeholder = heading;
+
+  let buttons = document.createElement("div");
+  buttons.className = "buttons";
+
+  let pop_Add = document.createElement("button");
+  pop_Add.id = "popupAdd";
+  pop_Add.textContent = "Add";
+
+  let popClose = document.createElement("button");
+  popClose.id = "popupClose";
+  popClose.textContent = "Close";
+
+  // add items to pop up
+  document.body.appendChild(pop);
+  pop.appendChild(pop_upHead);
+  pop.appendChild(popInput);
+  pop.appendChild(buttons);
+  buttons.appendChild(pop_Add);
+  buttons.appendChild(popClose);
 
 }
 
-
-//function for back button
-
-function hideTask(){
- add1.classList.add("hide")
-}
-
-//function for adding card
-
-function cardAdd(){
-  /* // to start
-   const start = () =>{
-      setTimeout(function(){
-         confetti.start();
-      }, 1000);
-   } 
-   //to end
-   const stop = () =>{
-      setTimeout(function(){
-         confetti.stop();
-      },5000);
-   } 
-
-   start();
-   stop();
-
-
-*/
-
-
-    //for main div
-    var newCard =document.createElement("div")
-    
-
-    //for heading
-     var title =document.createElement("h2")
-     title.classList.add("title")
-
-     //for list 
-     var listItem =document.createElement("div")
-     listItem.classList.add("Listitem")
-
-     //items
-     var item_num =document.createElement("div")
-     item_num.classList.add("item_num")
-     newCard.appendChild(item_num)
-     
-
-
-
-     //for deletebutton
-     var delBut =document.createElement("button")
-     delBut.classList.add("delbut")
-
-     //for adding  loist item button
-     var addItem=document.createElement("button")
-     addItem.classList.add("addbut")
-
-
-     newCard.appendChild(title)
-     newCard.appendChild(listItem)
-     newCard.appendChild(delBut)
-     newCard.appendChild(addItem)
-
-
-
-
-     cardCon.appendChild(newCard)
-
-     newCard.classList.add("card")
-
-     title.innerHTML=newCardName.value
-     delBut.innerHTML="Delete"
-     addItem.innerText="Add"
-     
-     delBut.addEventListener("click",function (){
-        newCard.remove()
-     })
-     //popup create
-
-    //P_BOX
-    addItem.addEventListener("click",function(){
-      var pop2=document.createElement("div")
-      pop2.classList.add("pop2")
-        newCard.appendChild(pop2)
-
-
-     //HEADING
-      var heading2=document.createElement("p")
-      heading2.classList.add("hd2")
-      pop2.appendChild(heading2)
-      heading2.innerHTML="ADD TASK"
-
-      
-    //input
-      var input= document.createElement("input")
-      input.classList.add("input")
-      pop2.appendChild(input)
-
-   //add button
-   var  add_1=document.createElement("button")
-   add_1.classList.add("add1")
-   pop2.appendChild(add_1)
-   add_1.innerHTML="ADD"
-
-
-   add_1.addEventListener("click",function(){
-      listItem.innerHTML=input.value
-       const done =document.createElement("button")
-       done.classList.add("done")
-       done.innerHTML="done"
-       listItem.appendChild(done)
-
-       done.addEventListener("click",function(){
-         listItem.style.textDecoration="linethrough"
-       })
-
-   })
-
-   //back
-    var back=document.createElement("button")
-    back.classList.add("close1")
-    pop2.appendChild(back)
-    back.innerHTML="BACK"
-   
- 
-    back.addEventListener("click",function (){
-      pop2.remove()
-   })
-
-
-
-
-
-
-
-
-
-     })
-
-
-
-
-     hideTask()
-
-     
-
- 
-}
-
-//const carditem= document.getElementById("newCardName1")
-//const addList= document.getElementById("add1")
-//const delitem= document.getElementById("close1")
-
-// second pop up...........
-
-//function bucket(){
-//listItem.innerHTML=carditem.value
-   
-//}
